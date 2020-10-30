405cf41781747a44b143a8c40273a6a4b777c321
Damon Holland <damonrholland@gmail.com>
2020-10-30 14:27:19 -0700

    Initial Commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+/*******************************************************************************
+File Name:    script.js
+Author:       Damon, Bob, Madeleine, Michael
+Date:         10/30/2020
+Class:        CS360
+Assignment:   Vibe Project
+Hours Worked: X hours
+Purpose:      Test Script
+*******************************************************************************/
+
+console.log("Test");
