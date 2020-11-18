c983f5f702879f4cd2b79fbae3b68c7229acbeae
Damon Holland <damonrholland@gmail.com>
2020-11-18 15:47:11 -0800

    Added Registration Validation. Entered fields are verified before being sent to database, if invalid an error message is sent to the user indicating required changes.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+const validateRegistration = require('../public/javascripts/validateRegistration');
+
+const validator = require('../public/javascripts/validateRegistration');
 
 /* GET sign in page. */
 router.get('/', function(req, res, next) {
@@ -13,33 +16,46 @@ router.get('/register', function(req, res, next) {
 });
 
 /* Handle Registration */
-router.post('/register', function(req, res) {
+router.post('/', function(req, res) {
+  const NO_ERRORS = "";
   //NEEDS CHANGED. Just set their uid to 10 for now. Not sure how we want to handle this.
-  var uid = 10;
-  var first_name = req.body.firstname;
-  var last_name = req.body.lastname;
-  var username = req.body.username;
-  var password = req.body.password;
-  var email = req.body.email;
+  let uid = 10;
+  let first_name = req.body.firstname;
+  let last_name = req.body.lastname;
+  let username = req.body.username;
+  let password = req.body.password;
+  let passwordconfirm = req.body.passwordconfirm;
+  let email = req.body.email;
+  
+  let error_message = validator.validateRegistration(first_name, last_name, username, password, passwordconfirm, email);
 
-  var newUser = new User();
-  newUser.uid = uid;
-  newUser.first_name = first_name;
-  newUser.last_name = last_name;
-  newUser.username = username;
-  newUser.password = password;
-  newUser.email = email;
+  if (NO_ERRORS == error_message) {
+    let newUser = new User();
+    newUser.uid = uid;
+    newUser.first_name = first_name;
+    newUser.last_name = last_name;
+    newUser.username = username;
+    newUser.password = password;
+    newUser.email = email;
+  
+    newUser.save(function(error, savedUser) {
+      if(error) {
+        console.log(error);
+        return res.status(500).send();
+      }
+      else {
+        res.render('signIn', { title: 'Sign in' });
+        return res.status(200).send();
+      }
+    })
+  }
+  else {
+    res.render('register', {title: 'Register',  errorbox: error_message });
+    console.log(error_message);
+    return res.status(200).send();
+  }
 
-  newUser.save(function(error, savedUser) {
-    if(error) {
-      console.log(error);
-      return res.status(500).send();
-    }
-    else {
-      return res.status(200).send();
-    }
-  })
-
+  
 });
 
 /* GET update account page. */
