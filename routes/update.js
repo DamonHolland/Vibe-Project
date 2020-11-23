29aef231a108f5ee0b51687b0346c78d66fdf669
Damon Holland <damonrholland@gmail.com>
2020-11-22 19:07:03 -0800

    Removed buttons from update page, Added routes for updating neccessary information. Will add seperate portions for updating password and security question

--- routes/update.js
+++ routes/update.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET update account page. */
 router.get('/', function(req, res, next) {
+  const FIELD_LABEL_EDIT = "Edit";
+
+
   if (!req.session.user){
     res.redirect('../login');
   }
@@ -13,7 +16,131 @@ router.get('/', function(req, res, next) {
       first: req.session.user.firstName,
       last: req.session.user.lastName,
       email: req.session.user.email,
-      user: req.session.user.username
+      user: req.session.user.username,
+      firstFieldDisabled: true,
+      lastFieldDisabled: true,
+      emailFieldDisabled: true,
+      userFieldDisabled: true,
+      firstFieldLabel: FIELD_LABEL_EDIT,
+      lastFieldLabel: FIELD_LABEL_EDIT,
+      emailFieldLabel: FIELD_LABEL_EDIT,
+      userFieldLabel: FIELD_LABEL_EDIT,
+    });
+  }
+});
+
+/* GET update first name page. */
+router.get('/firstName', function(req, res, next) {
+  const FIELD_LABEL_EDIT = "Edit";
+  const FIELD_LABEL_SAVE = "Save";
+
+
+  if (!req.session.user){
+    res.redirect('../login');
+  }
+  else{
+    console.log();
+    res.render('update', {
+      title: 'Update Account',
+      first: req.session.user.firstName,
+      last: req.session.user.lastName,
+      email: req.session.user.email,
+      user: req.session.user.username,
+      firstFieldDisabled: false,
+      lastFieldDisabled: true,
+      emailFieldDisabled: true,
+      userFieldDisabled: true,
+      firstFieldLabel: FIELD_LABEL_SAVE,
+      lastFieldLabel: FIELD_LABEL_EDIT,
+      emailFieldLabel: FIELD_LABEL_EDIT,
+      userFieldLabel: FIELD_LABEL_EDIT,
+    });
+  }
+});
+
+/* GET update last name page. */
+router.get('/lastName', function(req, res, next) {
+  const FIELD_LABEL_EDIT = "Edit";
+  const FIELD_LABEL_SAVE = "Save";
+
+
+  if (!req.session.user){
+    res.redirect('../login');
+  }
+  else{
+    console.log();
+    res.render('update', {
+      title: 'Update Account',
+      first: req.session.user.firstName,
+      last: req.session.user.lastName,
+      email: req.session.user.email,
+      user: req.session.user.username,
+      firstFieldDisabled: true,
+      lastFieldDisabled: false,
+      emailFieldDisabled: true,
+      userFieldDisabled: true,
+      firstFieldLabel: FIELD_LABEL_EDIT,
+      lastFieldLabel: FIELD_LABEL_SAVE,
+      emailFieldLabel: FIELD_LABEL_EDIT,
+      userFieldLabel: FIELD_LABEL_EDIT,
+    });
+  }
+});
+
+/* GET update email page. */
+router.get('/email', function(req, res, next) {
+  const FIELD_LABEL_EDIT = "Edit";
+  const FIELD_LABEL_SAVE = "Save";
+
+
+  if (!req.session.user){
+    res.redirect('../login');
+  }
+  else{
+    console.log();
+    res.render('update', {
+      title: 'Update Account',
+      first: req.session.user.firstName,
+      last: req.session.user.lastName,
+      email: req.session.user.email,
+      user: req.session.user.username,
+      firstFieldDisabled: true,
+      lastFieldDisabled: true,
+      emailFieldDisabled: false,
+      userFieldDisabled: true,
+      firstFieldLabel: FIELD_LABEL_EDIT,
+      lastFieldLabel: FIELD_LABEL_EDIT,
+      emailFieldLabel: FIELD_LABEL_SAVE,
+      userFieldLabel: FIELD_LABEL_EDIT,
+    });
+  }
+});
+
+/* GET update username page. */
+router.get('/username', function(req, res, next) {
+  const FIELD_LABEL_EDIT = "Edit";
+  const FIELD_LABEL_SAVE = "Save";
+
+
+  if (!req.session.user){
+    res.redirect('../login');
+  }
+  else{
+    console.log();
+    res.render('update', {
+      title: 'Update Account',
+      first: req.session.user.firstName,
+      last: req.session.user.lastName,
+      email: req.session.user.email,
+      user: req.session.user.username,
+      firstFieldDisabled: true,
+      lastFieldDisabled: true,
+      emailFieldDisabled: true,
+      userFieldDisabled: false,
+      firstFieldLabel: FIELD_LABEL_EDIT,
+      lastFieldLabel: FIELD_LABEL_EDIT,
+      emailFieldLabel: FIELD_LABEL_EDIT,
+      userFieldLabel: FIELD_LABEL_SAVE,
     });
   }
 });
