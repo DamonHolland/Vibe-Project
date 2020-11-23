3c397ea436ad375dff05bd8d00b5b2c430751c95
BobLStevens <stev4642@pacificu.edu>
2020-11-22 19:05:32 -0800

    Started displaying questions, not done

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Question = require('../models/question')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,13 @@ router.get('/', function(req, res, next) {
     res.redirect('/login');
   }
   else{
-    res.render('main');
+    var questions = Question.find({$query: {}, $orderby: {voteCount: -1}});
+
+    res.render('main', {
+      theQuestion: questions[0].theQuestion,
+      option1: questions[0].option1,
+      option2: questions[0].option2
+    });
   }
 });
 
