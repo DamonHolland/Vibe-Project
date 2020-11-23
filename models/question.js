e27810c5d1625dae6e321456b076422ca78bcccf
BobLStevens <stev4642@pacificu.edu>
2020-11-22 18:13:15 -0800

    Add Question functionality completed

--- models/question.js
+++ models/question.js
@@ -4,8 +4,11 @@ var Schema = mongoose.Schema;
 
 var QuestionSchema = new Schema(
   {
-    voteCount: {type: Int, required: true, default: 0},
+    voteCount: {type: Number, required: true, default: 0},
     creationDate: {type: Date, required: true, default: Date.now},
+    theQuestion: {type: String, required: true},
+    option1: {type: String, required: true},
+    option2: {type: String, required: true},
     author: {type: Schema.Types.ObjectId, ref: 'User', required: true, unique: true}
   }
 );
