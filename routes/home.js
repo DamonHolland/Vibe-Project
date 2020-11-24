a47eb3c0ce1e6183d485f4629ea08a1210a4b200
BobLStevens <stev4642@pacificu.edu>
2020-11-23 21:00:19 -0800

    Added logic for no reanswering, not done

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Question = require('../models/question');
+var Answer = require('../models/answer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,19 +9,44 @@ router.get('/', function(req, res, next) {
     res.redirect('/login');
   }
   else{
-    var query = Question.find({$query: {}, $orderby: {voteCount: -1}});
+    var answerQuery = Answer.find({_id: req.session.user._id});
 
-    query.exec(function(err, questions) {
-      if(err)
-      {
-        console.log(err);
+    answerQuery.exec(function(answerErr, answers) {
+      if (answerErr) {
+        console.log(answerErr);
       }
       else {
-        req.session.qid = questions[0]._id;
-        res.render('main', {
-          theQuestion: questions[0].theQuestion,
-          option1: questions[0].option1,
-          option2: questions[0].option2
+        var questionQuery = Question.find({}).sort({voteCount: -1});
+
+        questionQuery.exec(function(questionErr, questions) {
+          if(questionErr)
+          {
+            console.log(questionErr);
+          }
+          else {
+            var found = false;
+            for (var i = 0; i < questions.length && !found; ++i) {
+              var contained = false;
+              for (var j = 0; j < answers.length && !contained; ++j) {
+                console.log(`${answers[j].question} == ${question[i]._id} = ${answers[j].question == question[i]._id}`)
+                if(answers[j].question == question[i]._id) {
+                  contained = true;
+                }
+              }
+              if (!contained) {
+                req.session.question = questions[i];
+                found = true;
+                res.render('main', {
+                  theQuestion: questions[i].theQuestion,
+                  option1: questions[i].option1,
+                 option2: questions[i].option2
+                });
+              }
+            }
+            if(!found) {
+              res.redirect('/addQuestion');
+            }
+          }
         });
       }
     });
@@ -29,7 +55,21 @@ router.get('/', function(req, res, next) {
 
 /* Submit Answer */
 router.post('/', function(req, res, next) {
-    res.redirect('results');
+  if ("Submit" == req.body.buttons)
+  {
+    if(!req.body.options)
+    {
+      res.redirect('/');
+    }
+    else {
+      Answer.create({
+        user: req.session.user._id,
+        question: req.session.question._id,
+        answer: req.body.options
+      });
+    }
+  }
+  res.redirect('results');
 });
 
 module.exports = router;
